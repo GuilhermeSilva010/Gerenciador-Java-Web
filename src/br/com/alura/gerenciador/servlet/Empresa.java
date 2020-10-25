a803a74ecf9fb61a64d4a0cc05899fc4779d7dd4
Guilherme Carvalho <carvalhog136@gmail.com>
2020-10-25 12:30:47 -0300

    Primeito projeto Web com Java usando Servlets

---
+++ src/br/com/alura/gerenciador/servlet/Empresa.java
@@ -0,0 +1,31 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.Date;
+
+public class Empresa {
+
+	private String nome;
+	private Integer id;
+	private Date dataAbertura = new Date();
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	
+}
